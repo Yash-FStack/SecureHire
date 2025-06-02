3e083aa185eab93d06a2349e5a49b32344f75000
code-x-yash <yashrajwanshii@gmail.com>
2025-06-02 17:00:45 +0530

    Connected WebPOC with SDK

--- SecureHire/Controllers/AssessmentController.cs
+++ SecureHire/Controllers/AssessmentController.cs
@@ -1,18 +1,24 @@
 ﻿using CheatingProofInterviewSystem.Models;
-using SecureHire.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using SecureHire.Services;
-using System.Threading.Tasks;
 using CheatingProofInterviewSystem.Services;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using SecureHire.Services;
+using SecureHire.Services;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace CheatingProofInterviewSystem.Controllers
 {
-    [Authorize(Roles = "Candidate")]
     public class AssessmentController : Controller
     {
         private readonly IAssessmentService _assessmentService;
-
+        [Inject] IJSRuntime JS { get; set; }
         public AssessmentController(IAssessmentService assessmentService)
         {
             _assessmentService = assessmentService;
@@ -31,13 +37,18 @@ namespace CheatingProofInterviewSystem.Controllers
 
         // GET: /Assessment/Take/5
         [HttpGet]
-        public async Task<IActionResult> Take(int id)
+        public async Task<IActionResult> Take(int id = 1, bool flag=true)
         {
-            var examSession = await _assessmentService.StartSessionAsync(User.Identity.Name, id);
-            if (examSession == null)
-                return NotFound();
+            if (flag)
+            {
+                var examSession = await _assessmentService.StartSessionAsync(User.Identity.Name, id);
+                if (examSession == null)
+                    return NotFound();
 
-            return View(examSession);
+                return View(examSession);
+            }
+            else
+                return null;
         }
         
 
@@ -54,13 +65,38 @@ namespace CheatingProofInterviewSystem.Controllers
             return RedirectToAction("Dashboard", "Candidate");
         }
 
-
-
         [HttpGet]
         public IActionResult PreTake()
         {
             
             return View("PreCheck");
+        }
+
+        [HttpGet]
+        public async Task<int> CheckProcess()
+        {
+            try
+            {
+                var _connectionString = "server = 23.101.140.148; user id = dnh2020; password = mcn@123; database = SecureHire; TrustServerCertificate = True;";
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    var email = "yash@gmail.com";
+                    var sql = "SELECT ProcessesKilled FROM ProctoringEvents WHERE Email = @Email";
+
+                    var result = await connection.QueryAsync<int>(sql, new { Email = email });
+
+                    int processesKilled = result.FirstOrDefault(); // Safely get the first result (or 0)
+
+                    return processesKilled;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
         }
     }
 }
